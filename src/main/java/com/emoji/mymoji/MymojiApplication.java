dacd65297939fe2e93288f041edc4c363eec906d
park sehwan <58384653+sehwan12@users.noreply.github.com>
2025-11-07 21:36:09 +0900

    Initial commit

---
+++ src/main/java/com/emoji/mymoji/MymojiApplication.java
@@ -0,0 +1,13 @@
+package com.emoji.mymoji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymojiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymojiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emoji/mymoji/MymojiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emoji.mymoji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymojiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
