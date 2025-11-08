db647a06097e4f802d8780057807c72698392081
park sehwan <58384653+sehwan12@users.noreply.github.com>
2025-11-08 09:11:21 +0900

    fix/ api 링크 수정fix

--- src/main/java/com/emoji/mymoji/service/GeminiService.java
+++ src/main/java/com/emoji/mymoji/service/GeminiService.java
@@ -26,7 +26,8 @@ public class GeminiService {
 
     // 3. Google AI API 엔드포인트
     private static final String GEMINI_API_URL =
-            "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=";
+            "https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=";
+
 
 
     /**
